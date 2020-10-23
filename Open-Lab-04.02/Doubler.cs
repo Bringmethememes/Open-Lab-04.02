dc73d35f2ec0a08a53691bab323bdb000eb3cfa8
PC <PC@ANDREJKOVPOITA>
2020-10-23 20:25:59 +0200

    Death

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,26 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            char[] bruh = new char[original.Length];
+            for (int i = 0; i < original.Length; i++)
+            {
+                bruh[i] = original[i];
+            }
+            string wut = string.Join("", bruh);
+            wut = null;
+            foreach (char k in original)
+            {
+                wut = wut + k + k;
+            }
+            return wut;
+
+
+
+            
+
+
+
         }
+
     }
 }
